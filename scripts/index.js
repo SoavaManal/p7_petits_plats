89bf6310a931756de59ecbded98f973add7e2824
Manal Soava <manal.hibat.allah@gmail.com>
2022-12-06 07:05:28 +0100

    mettre a jour des recherche avancé aprés chaque tag

--- scripts/index.js
+++ scripts/index.js
@@ -101,10 +101,12 @@ const searchPrincipale = (tag) => {
     main.innerHTML = `<div><p class='text-danger'> Aucune recette ne correspond à votre critère… vous pouvez
     chercher « tarte aux pommes », « poisson », etc.</div>`;
   }
+  return recipes_try;
 };
 
 // addEventListener
 
+// lancer la recherche en click sur la loop
 search.addEventListener("click", () => {
   if (tag_search.value.length > 2) {
     searchPrincipale(tag_search.value);
@@ -113,71 +115,112 @@ search.addEventListener("click", () => {
     tag_search.value = "";
   }
 });
+// lancer la recherche pour chaque nouveau caractéres
 tag_search.addEventListener("input", () => {
   if (tag_search.value.length > 2) {
     searchPrincipale(tag_search.value);
   }
 });
 
+let array = [];
+let tag = [];
+let array_igr = [];
+let array_ust = [];
+let array_app = [];
+
 select_igr.addEventListener("change", (e) => {
-  let array = [];
-  let array_igr = [];
-  let tag = e.target.value;
+  tag.push(e.target.value);
 
-  mot_cle.innerHTML += `<button class="btn btn-primary m-1">
-  ${tag}<i class="bi bi-x-circle"></i></button>`;
+  mot_cle.innerHTML += `<div class="btn btn-primary m-1">
+      ${e.target.value}<span class="close"><i class="bi bi-x-circle"></i><span></div>`;
 
-  console.log(tag);
-  for (let i = 0; i < recipes.length; i++) {
-    for (let j = 0; j < recipes[i].ingredients.length; j++)
-      if (
-        recipes[i].ingredients[j].ingredient
-          .toLowerCase()
-          .includes(tag.toLowerCase())
-      ) {
-        array.push(recipes[i]);
-      }
+  const btnClose = document.querySelector(".close");
+  for (let t of tag) {
+    btnClose.addEventListener("click", () => {
+      tag.splice(t);
+    });
   }
-  console.log(array);
-  for (let k = 0; k < array.length; k++) {
-    for (let l = 0; l < array[k].ingredients.length; l++) {
-      if (
-        array[k].ingredients[l].ingredient !== tag &&
-        !array_igr.includes(array[k].ingredients[l].ingredient)
-      ) {
-        array_igr.push(array[k].ingredients[l].ingredient);
+  console.log(tag);
+
+  for (let recipe of recipes) {
+    for (let igr of recipe.ingredients) {
+      for (let t = 0; t < tag.length; t++) {
+        if (igr.ingredient == tag[t]) {
+          array.push(recipe);
+        }
       }
     }
   }
-  console.log(array_igr);
+  console.log(array);
+  for (let l = 0; l < array.length; l++) {
+    if (array[l].appliance !== tag && !array_app.includes(array[l].appliance)) {
+      array_app.push(array[l].appliance);
+    }
+    for (let m = 0; m < array[l].ingredients.length; m++) {
+      if (!array_igr.includes(array[l].ingredients[m].ingredient)) {
+        array_igr.push(array[l].ingredients[m].ingredient);
+      }
+    }
+    for (let n = 0; n < array[l].ustensils.length; n++) {
+      if (!array_ust.includes(array[l].ustensils[n])) {
+        array_ust.push(array[l].ustensils[n]);
+      }
+    }
+  }
+  console.log(array_app);
   main.innerHTML = "";
   displayData(array);
   select_igr.innerHTML = "";
+  select_ust.innerHTML = "";
+  select_app.innerHTML = "";
   displayIngredient(array_igr);
+  displayUst(array_ust);
+  displayApp(array_app);
 });
 
 select_ust.addEventListener("change", (e) => {
-  let array = [];
-  let tag = e.target.value;
+  tag.push(e.target.value);
 
-  mot_cle.innerHTML += `<button class="btn btn-danger m-1">
-  ${tag}<i class="bi bi-x-circle"></i></button>`;
+  mot_cle.innerHTML += `<dicv class="btn btn-danger m-1">
+  ${e.target.value}<i class="bi bi-x-circle"></i></div>`;
 
   console.log(tag);
   for (let i = 0; i < recipes.length; i++) {
     for (let j = 0; j < recipes[i].ustensils.length; j++) {
-      if (recipes[i].ustensils[j].toLowerCase().includes(tag.toLowerCase())) {
-        array.push(recipes[i]);
+      for (let t of tag) {
+        if (recipes[i].ustensils[j].toLowerCase().includes(t.toLowerCase())) {
+          array.push(recipes[i]);
+        }
       }
     }
   }
-  console.log(array);
+  for (let l = 0; l < array.length; l++) {
+    if (array[l].appliance !== tag && !array_app.includes(array[l].appliance)) {
+      array_app.push(array[l].appliance);
+    }
+    for (let m = 0; m < array[l].ingredients.length; m++) {
+      if (!array_igr.includes(array[l].ingredients[m].ingredient)) {
+        array_igr.push(array[l].ingredients[m].ingredient);
+      }
+    }
+    for (let n = 0; n < array[l].ustensils.length; n++) {
+      if (!array_ust.includes(array[l].ustensils[n])) {
+        array_ust.push(array[l].ustensils[n]);
+      }
+    }
+  }
+  console.log(array_app);
   main.innerHTML = "";
   displayData(array);
+  select_igr.innerHTML = "";
+  select_ust.innerHTML = "";
+  select_app.innerHTML = "";
+  displayIngredient(array_igr);
+  displayUst(array_ust);
+  displayApp(array_app);
 });
 
 select_app.addEventListener("change", (e) => {
-  let array = [];
   let tag = e.target.value;
 
   mot_cle.innerHTML += `<button class="btn btn-success m-1">
@@ -189,17 +232,30 @@ select_app.addEventListener("change", (e) => {
       array.push(recipes[i]);
     }
   }
+  for (let l = 0; l < array.length; l++) {
+    if (array[l].appliance !== tag && !array_app.includes(array[l].appliance)) {
+      array_app.push(array[l].appliance);
+    }
+    for (let m = 0; m < array[l].ingredients.length; m++) {
+      if (!array_igr.includes(array[l].ingredients[m].ingredient)) {
+        array_igr.push(array[l].ingredients[m].ingredient);
+      }
+    }
+    for (let n = 0; n < array[l].ustensils.length; n++) {
+      if (!array_ust.includes(array[l].ustensils[n])) {
+        array_ust.push(array[l].ustensils[n]);
+      }
+    }
+  }
+  console.log(array_app);
   main.innerHTML = "";
   displayData(array);
-  //let array_apl = [];
-  // for (let j = 0; j < array.length; j++) {
-  //   if (tag !== array[j].appliance) {
-  //     array_apl.push(array[j].appliance);
-  //   }
-  // }
-  // console.log(array_apl);
-  // select_app.innerHTML = "";
-  // displayApp(array_apl);
+  select_igr.innerHTML = "";
+  select_ust.innerHTML = "";
+  select_app.innerHTML = "";
+  displayIngredient(array_igr);
+  displayUst(array_ust);
+  displayApp(array_app);
 });
 
 // input
